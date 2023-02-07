dc0547bdbfd35c5295f6023ff03f51253a62efa9
Karl Johann Lattikas <lattikaskarljohann@gmail.com>
2023-02-07 15:16:17 +0200

    Test commiting from Windows

--- api/urls.py
+++ api/urls.py
@@ -7,3 +7,5 @@ urlpatterns = [
     path('invoices/<int:pk>/invoicerows/', views.InvoiceRowsByInvoice.as_view()),
     path('invoicerows/<int:pk>/', views.InvoiceRowDetail.as_view())
 ]
+
+# comment from PC
